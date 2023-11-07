2506472bb61e7339b763ca2353bc3eaebc2526e1
maryferrer <mary.ferrer6@gmail.com>
2023-11-07 15:08:48 -0800

    feat: create header component

---
+++ src/Header.tsx
@@ -0,0 +1,16 @@
+
+const Header = () => {
+
+    return (
+    <div>
+        <h1>Mary Ferrer</h1>
+        <ul>
+            <li>Projects</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+    )
+}
+
+export default Header;
