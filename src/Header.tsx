c4280fd06a551804adbc39c58ff37ebf15c06a32
maryferrer <mary.ferrer6@gmail.com>
2023-11-08 22:17:21 -0800

    feat: mobile nav skeleton

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,14 +1,15 @@
-
-const Header = () => {
+const Header = (isMobile: any) => {
 
     return (
     <div>
         <h1>Mary Ferrer</h1>
-        <ul>
-            <li>Projects</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
+        
+            {isMobile ? <div><span></span><span></span><span></span></div> : 
+            <ul>
+                <li><a>Projects</a></li>
+            <li><a>About</a></li>
+            <li><a>Contact</a></li>
+            </ul> }
     </div>
     )
 }
